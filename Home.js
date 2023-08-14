ab592d3627e9cf377e9cb3ee57abeea16a37159a
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-08-14 22:55:36 +0300

    updating and fixing

--- Home.js
+++ Home.js
@@ -1,4 +1,14 @@
+
+//equals sign
+
 const equals=document.querySelector('.equals')
-equals.style.backgroundColor='blue'
+equals.style.backgroundColor='green'
 equals.style.padding='2px'
+equals.style.border='none'
 equals.style.color='white'
+
+//AC
+const myAc=document.querySelector('.AC')
+myAc.style.backgroundColor='red'
+myAc.style.color='white'
+myAc.style.border='none'
